6734a1f35471b87931607c259a47c5b746251902
rv328 <superpeko@yahoo.com>
2018-04-08 20:16:25 +0200

    Updated the package.json file

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ class App extends Component{
     render(){
         return(
             <div>
-                <h1>My React App Boilerplate</h1>
+                <h1>My React App Boilerplate!</h1>
             </div>
         )
     }
